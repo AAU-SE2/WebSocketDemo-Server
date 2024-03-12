c3f7057d8dd8935784f9c96ee2d4345110c5e479
djamn <david@jamnig.net>
2024-03-12 20:44:06 +0100

    small fix testing ci

--- src/main/java/at/aau/serg/websocketdemoserver/websocket/handler/WebSocketHandlerConfig.java
+++ src/main/java/at/aau/serg/websocketdemoserver/websocket/handler/WebSocketHandlerConfig.java
@@ -1,7 +1,9 @@
 package at.aau.serg.websocketdemoserver.websocket.handler;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocket
