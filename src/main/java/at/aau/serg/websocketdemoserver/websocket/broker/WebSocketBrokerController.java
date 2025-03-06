5295bfcc0673860faf77b3673e9b293f2c3f34c8
bthaus <bwuest@gmx.at>
2025-03-06 20:22:55 +0100

    Add Broker routes

--- src/main/java/at/aau/serg/websocketdemoserver/websocket/broker/WebSocketBrokerController.java
+++ src/main/java/at/aau/serg/websocketdemoserver/websocket/broker/WebSocketBrokerController.java
@@ -1,18 +1,25 @@
 package at.aau.serg.websocketdemoserver.websocket.broker;
 
+import at.aau.serg.websocketdemoserver.messaging.dtos.StompMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
+
 
 @Controller
 public class WebSocketBrokerController {
-
     @MessageMapping("/hello")
     @SendTo("/topic/hello-response")
-    public String handleHello(String message) {
+    public String handleHello(String text) {
         // TODO handle the messages here
-        return "echo from broker: " + HtmlUtils.htmlEscape(message);
+        return "echo from broker: "+text;
+    }
+    @MessageMapping("/object")
+    @SendTo("/topic/rcv-object")
+    public StompMessage handleObject(StompMessage msg) {
+
+       return msg;
     }
 
 }
