1da206aafaa2c70539b0de7262c43b97497ebe21
Damaris <damarishi@edu.aau.at>
2025-04-05 16:00:13 +0200

    Event für Risiko und Bank differenziert

--- src/main/java/at/aau/serg/websocketdemoserver/dkt/tiles/Event.java
+++ src/main/java/at/aau/serg/websocketdemoserver/dkt/tiles/Event.java
@@ -3,13 +3,22 @@ package at.aau.serg.websocketdemoserver.dkt.tiles;
 import at.aau.serg.websocketdemoserver.dkt.Tile;
 
 public class Event extends Tile {
-
         // What does this do?
 
         public Event(int position, String name) {
             super(position, name);
         }
 
-
+        @Override
+        public String getTileType(){
+            String name = getName().toLowerCase();
+            if (name.contains("risiko")) {
+                return "event_risiko";
+            } else if (name.contains("bank")){
+                return "event_bank";
+            } else {
+                return "event";
+            }
+        }
 
 }
